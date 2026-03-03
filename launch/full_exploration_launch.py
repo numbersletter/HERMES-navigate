d6af0e6576112a41c33883500d02458fbb790a7e
Yomal <41592985+numbersletter@users.noreply.github.com>
2026-03-02 18:55:44 -0800

    Update copyright year to 2026

--- launch/full_exploration_launch.py
+++ launch/full_exploration_launch.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright 2024 HERMES Team
+# Copyright 2026 HERMES Team
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
