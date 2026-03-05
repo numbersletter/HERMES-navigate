80063a9b702173cac864eafc9e780069c7a6b7ba
Yomal Perera <yperera@uci.edu>
2026-03-05 11:26:29 -0800

    Add a launch to display robot, changed simulated robot parameters. Changed the bridge config so only necessary topics to closesly mimic real setup are shown (just clock, cmd_vel, scan), which means the diff_drive plugin (or the real controller) does not output odometry. All odometry from rf2o (lidar) which publishes tf odom->base_link and odometry over /odom_rf2o (configurable). When we do the full simulation, we may need to reconfigure rf2o so the odometry messages are passed in /odom.

---
+++ launch/display_launch.py
@@ -0,0 +1,49 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, LaunchConfiguration
+from launch_ros.actions import Node
+from ros_gz_bridge.actions import RosGzBridge
+from ros_gz_sim.actions import GzServer
+
+
+def generate_launch_description():
+    pkg_share = get_package_share_directory('hermes_navigate')
+    ros_gz_sim_share = get_package_share_directory('ros_gz_sim')
+    gz_spawn_model_launch_source = os.path.join(ros_gz_sim_share, "launch", "gz_spawn_model.launch.py")
+    default_model_path = os.path.join(pkg_share, 'urdf', 'hermes_sim.sdf.xacro')
+    default_rviz_config_path = os.path.join(pkg_share, 'config', 'config.rviz')
+    world_path = os.path.join(pkg_share, 'worlds', 'my_world.sdf')
+    bridge_config_path = os.path.join(pkg_share, 'config', 'bridge_config.yaml')
+
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}, {'use_sim_time': LaunchConfiguration('use_sim_time')}]
+    )
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+        parameters=[{'robot_description': Command(['xacro ', default_model_path])}]
+    )
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', LaunchConfiguration('rvizconfig')],
+    )
+
+    return LaunchDescription([
+        DeclareLaunchArgument(name='model', default_value=default_model_path, description='Absolute path to robot model file'),
+        DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path, description='Absolute path to rviz config file'),
+        DeclareLaunchArgument(name='use_sim_time', default_value='True', description='Flag to enable use_sim_time'),
+        robot_state_publisher_node,
+        joint_state_publisher_node,
+        rviz_node,
+    ])
